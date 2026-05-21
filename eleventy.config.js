d02580d7b707d92e2b023cb1cc0343e02cba4cec
Mariano S. Cosentino <12953567+mok726@users.noreply.github.com>
2026-05-21 18:44:04 +0000

    Agrega soporte para imagenes y logos

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("content/css");
+  eleventyConfig.addPassthroughCopy("imagenes");
+
 
   eleventyConfig.addCollection("activities", function(collectionApi) {
     return collectionApi.getFilteredByGlob("content/activities/*.md");
