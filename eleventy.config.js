4be3d2f4dfd58b1caf3faa7a8cfa5760168127f1
Mariano S. Cosentino <12953567+mok726@users.noreply.github.com>
2026-05-21 15:25:41 +0000

    Agrego css y formato inicial

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
 
+  eleventyConfig.addPassthroughCopy("content/css");
+
   eleventyConfig.addCollection("activities", function(collectionApi) {
     return collectionApi.getFilteredByGlob("content/activities/*.md");
   });
