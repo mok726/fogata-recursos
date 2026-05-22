c134f467fee85b3d2c5632bec3af5d4cb72c8355
Marianok <mok726@users.noreply.github.com>
2026-05-22 09:19:17 -0300

    Update badges.js

--- content/_data/badges.js
+++ content/_data/badges.js
@@ -1,25 +1,87 @@
 module.exports = function() {
 
   return [
+    
     {
-      id: "polinizadores",
-      name: "Polinizadores"
+      id: "agricultura",
+      name: "agricultura"
     },
 
     {
       id: "bosques",
-      name: "Bosques"
+      name: "bosques"
+    },
+
+    {
+      id: "genero",
+      name: "genero"
+    },
+
+    {
+      id: "nutricion",
+      name: "nutricion"
+    },
+
+    {
+      id: "ptt",
+      name: "ptt"
     },
 
     {
       id: "agua",
-      name: "Agua"
+      name: "agua"
+    },
+
+    {
+      id: "gosolar",
+      name: "gosolar"
+    },
+
+    {
+      id: "cambioclimatico",
+      name: "cambioclimatico"
+    },
+
+    {
+      id: "gobernanza",
+      name: "gobernanza"
+    },
+
+    {
+      id: "oceano",
+      name: "oceano"
+    },
+
+    {
+      id: "rrd",
+      name: "rrd"
     },
 
     {
       id: "biodiversidad",
-      name: "Biodiversidad"
+      name: "biodiversidad"
+    },
+
+    {
+      id: "energia",
+      name: "energia"
+    },
+
+    {
+      id: "hambre",
+      name: "hambre"
+    },
+
+    {
+      id: "polinizadores",
+      name: "polinizadores"
+    },
+
+    {
+      id: "suelos",
+      name: "suelos"
     }
+
 
   ];
 
