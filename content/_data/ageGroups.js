7261f6cbf964722c455ebe54afe4ad4bd9caefec
Mariano S. Cosentino <12953567+mok726@users.noreply.github.com>
2026-05-22 10:13:15 +0000

    Agregando templates y formatos varios

---
+++ content/_data/ageGroups.js
@@ -0,0 +1,29 @@
+module.exports = function() {
+
+  return [
+
+    {
+      id: "manada",
+      name: "Manada"
+    },
+
+    {
+      id: "unidad-scout",
+      name: "Unidad Scout"
+    },
+
+    {
+      id: "caminantes",
+      name: "Caminantes"
+    },
+
+    {
+      id: "rovers",
+      name: "Rovers"
+    }
+
+  ];
+
+};
+
+
